0696b63dd428ece9b3a122a8b22fdbca280e2758
Portela03 <joao.portela03@gmail.com>
2023-09-12 15:01:44 -0300

    first commit

---
+++ app/src/main/java/com/example/aulajetpackstage/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.aulajetpackstage
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aulajetpackstage.ui.theme.AulaJetPackStageTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AulaJetPackStageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    var vlrDoTextField by remember {
+                        mutableStateOf("")
+                    }
+
+
+                    Column() {
+                       TextField(value = vlrDoTextField, onValueChange ={
+                           vlrDoTextField = it;
+                           Log.i("####",vlrDoTextField)
+                       })
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AulaJetPackStageTheme {
+        Greeting("Android")
+    }
+}
